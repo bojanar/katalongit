ad91a05477ebd0b1aa07a9ba42c6a68128888b17
Bojana Reljic <bojana.reljic@icloud.com>
2017-12-15 11:44:36 +0100

    recorded and renamed test case Google to OrangeHRM

---
+++ Test Suites/Iteration1.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>all TCs from iteration1</description>
+   <name>Iteration1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b686fcea-d7ef-4fdf-9232-385b2045508d</testSuiteGuid>
+   <testCaseLink>
+      <guid>b1659ab2-2796-44b6-8282-8078a84a61d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestBlic</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a0564f28-6885-442e-99de-40fd0d96ec60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOrangeHRM</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e3f129a3-3842-46d9-929f-991b5de06e11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestRTS</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
